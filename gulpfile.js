14b97c1eb82a8e301e14ad32212a627e441d49aa
lrg1980 <admin@binweb.net>
2019-04-14 17:14:25 -0300

    Sumando Gulpfile para manejar sass automatico

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const { watch, series, src, dest } = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer')
+
+function sassAndAuto () {
+     return src('scss/app.scss')
+          .pipe(autoprefixer({
+               browsers: ['last 2 versions'],
+               cascade: false
+          }))
+          .pipe(sass({
+               includePaths: ['scss']
+          }))
+          .pipe(dest('css'));
+}
+
+// eslint-disable-next-line semi
+exports.sassAndAuto = sassAndAuto;
+// eslint-disable-next-line semi
+exports.default = series(sassAndAuto);
+// eslint-disable-next-line semi
+watch(['scss/*.scss'], series(sassAndAuto));
