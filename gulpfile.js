317ec6369c754847b3947a7e1af04ac41e49975c
lrg1980 <admin@binweb.net>
2019-04-14 17:42:30 -0300

    Agregando variables y css en Header

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
-const { watch, series, src, dest } = require('gulp');
-const sass = require('gulp-sass');
+const { watch, series, src, dest } = require('gulp')
+const sass = require('gulp-sass')
 const autoprefixer = require('gulp-autoprefixer')
 
 function sassAndAuto () {
-     return src('scss/app.scss')
-          .pipe(autoprefixer({
-               browsers: ['last 2 versions'],
-               cascade: false
-          }))
-          .pipe(sass({
-               includePaths: ['scss']
-          }))
-          .pipe(dest('css'));
+  return src('scss/app.scss')
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(sass({
+      includePaths: ['scss']
+    }))
+    .pipe(dest('css'))
 }
 
 // eslint-disable-next-line semi
